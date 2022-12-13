9eb1e837bc0d1aa6a1c15730d6f545437c2a78f2
Fredrik Jonsén <frjonsen@users.noreply.github.com>
2022-12-13 21:47:01 +0100

    Reorganize setup

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,30 @@ struct Topography {
 #[derive(Component)]
 struct Altitude(u8);
 
+#[derive(Component)]
+struct Position(Vec2);
+
+#[derive(Resource)]
+struct InputMap(Vec<Vec<u8>>);
+
 #[derive(Bundle)]
 struct Point {
     altitude: Altitude,
+    position: Position,
     mesh: MaterialMesh2dBundle<ColorMaterial>,
 }
 
 const WORLD_SIZE: f32 = 1200.;
+
+#[derive(Resource)]
+struct TileSize(f32);
+
+impl FromWorld for TileSize {
+    fn from_world(world: &mut World) -> Self {
+        let input_map = world.get_resource::<InputMap>().unwrap();
+        Self(WORLD_SIZE / input_map.0.len().max(input_map.0[0].len()) as f32)
+    }
+}
 
 fn parse_input() -> Vec<Vec<u8>> {
     let input = include_str!("../input.txt");
@@ -39,15 +56,18 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
-                title: "A*".to_string(),
-                width: WORLD_SIZE / 2.,
-                height: WORLD_SIZE,
+                title: "A* Visualizer".to_string(),
+                width: WORLD_SIZE,
+                height: WORLD_SIZE / 2.,
                 present_mode: PresentMode::AutoVsync,
                 ..default()
             },
             ..default()
         }))
+        .insert_resource(InputMap(parse_input()))
+        .init_resource::<TileSize>()
         .add_startup_system(setup)
+        .add_system(bevy::window::close_on_esc)
         // .add_plugin(WorldInspectorPlugin::new())
         .run();
 }
@@ -56,35 +76,39 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    map: Res<InputMap>,
+    tile_size: Res<TileSize>,
 ) {
     commands.spawn(Camera2dBundle::new_with_far(WORLD_SIZE));
-    let topography = parse_input();
-    let tile_size = WORLD_SIZE / topography.len().max(topography[0].len()) as f32;
+    let topography = &map.0;
 
     let mut rows: Vec<Vec<Point>> = Vec::new();
-    for (y, line) in topography.into_iter().enumerate() {
+    for (y, line) in topography.iter().enumerate() {
         let mut parsed_line: Vec<Point> = Vec::new();
-        for (x, a) in line.into_iter().enumerate() {
+        for (x, a) in line.iter().enumerate() {
             let color = match a {
                 254 => Color::rgb_u8(255, 0, 0),
                 255 => Color::rgb_u8(0, 255, 0),
                 _ => Color::rgb_u8(0, 0, 255 / 27 * a),
             };
+            let position = Vec2::new(x as f32, y as f32);
+
             let mesh = MaterialMesh2dBundle {
                 mesh: meshes
-                    .add(shape::Quad::new(Vec2::new(tile_size, tile_size)).into())
+                    .add(shape::Quad::new(Vec2::new(tile_size.0, tile_size.0)).into())
                     .into(),
                 material: materials.add(ColorMaterial::from(color)),
                 transform: Transform::from_translation(Vec3::new(
-                    y as f32 * tile_size,
-                    x as f32 * tile_size,
+                    position.x * tile_size.0,
+                    position.y * tile_size.0,
                     0.,
                 )),
                 ..default()
             };
             let p = Point {
-                altitude: Altitude(a),
+                altitude: Altitude(*a),
                 mesh,
+                position: Position(position),
             };
             parsed_line.push(p);
         }
@@ -93,8 +117,8 @@ fn setup(
 
     let topography = Topography {
         spatial: SpatialBundle::from_transform(Transform::from_translation(Vec3::new(
-            -WORLD_SIZE / 4.,
-            -WORLD_SIZE / 2.,
+            -WORLD_SIZE / 2. + tile_size.0 / 2.,
+            -WORLD_SIZE / 4. + tile_size.0 / 2.,
             0.,
         ))),
     };
